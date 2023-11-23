4869c288e029e7bc4a562d8201f3eb4e23345451
BoredApe8461 <letesinkelegesse@gmail.com>
2023-11-23 08:21:50 +0100

    fetch asynchronously

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ struct WeightConsumption {
     /// The percentage of the weight used by user operational dispatches compared to the
     /// maximum potential.
     operational: f32,
-    /// The percentage of the weight used by the mandatory tasks of a parachain compared 
+    /// The percentage of the weight used by the mandatory tasks of a parachain compared
     /// to the maximum potential.
     mandatory: f32,
 }
@@ -31,26 +31,49 @@ impl std::fmt::Display for WeightConsumption {
     }
 }
 
-fn parachains() -> Vec<Parachain> {    
+fn parachains() -> Vec<Parachain> {
     vec![
-    Parachain { name: "Moonbeam".to_string(), rpc_url: "wss://moonbeam.public.blastapi.io".to_string(), para_id: 2004 },
-    Parachain { name: "Acala".to_string(), rpc_url: "wss://acala-polkadot.api.onfinality.io/public-ws".to_string(), para_id: 2000 },
-    Parachain { name: "Astar".to_string(), rpc_url: "wss://astar.api.onfinality.io/public-ws".to_string(), para_id: 2006 },
+        Parachain {
+            name: "Acala".to_string(),
+            rpc_url: "wss://acala-polkadot.api.onfinality.io/public-ws".to_string(),
+            para_id: 2000,
+        },
+        Parachain {
+            name: "Moonbeam".to_string(),
+            rpc_url: "wss://moonbeam.public.blastapi.io".to_string(),
+            para_id: 2004,
+        },
+        Parachain {
+            name: "Astar".to_string(),
+            rpc_url: "wss://astar.api.onfinality.io/public-ws".to_string(),
+            para_id: 2006,
+        },
     ]
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    for para in parachains() {
-        let consumption = weight_consumption(para.rpc_url.as_str()).await?;
+    let tasks: Vec<_> = parachains()
+        .into_iter()
+        .map(|para| tokio::spawn(async move { log_weight_consumption(para).await }))
+        .collect();
 
-        print!("{}: \n{}", para.name, consumption);
+    for task in tasks {
+        task.await.unwrap();
     }
 
     Ok(())
 }
 
-async fn weight_consumption(rpc_url: &str) -> Result<WeightConsumption, Box<dyn std::error::Error>> {
+async fn log_weight_consumption(para: Parachain) {
+    if let Ok(consumption) = weight_consumption(&para.rpc_url).await {
+        println!("{}: \n{}", para.name, consumption);
+    }
+}
+
+async fn weight_consumption(
+    rpc_url: &str,
+) -> Result<WeightConsumption, Box<dyn std::error::Error>> {
     let api = OnlineClient::<PolkadotConfig>::from_url(rpc_url).await?;
 
     let weight_query = polkadot::storage().system().block_weight();
@@ -62,7 +85,7 @@ async fn weight_consumption(rpc_url: &str) -> Result<WeightConsumption, Box<dyn 
         .await?
         .ok_or("Failed")?;
 
-    let weight_limit_query =  polkadot::constants().system().block_weights();
+    let weight_limit_query = polkadot::constants().system().block_weights();
     let weight_limit = api.constants().at(&weight_limit_query)?;
 
     let weight_limit = weight_limit.max_block.ref_time;
@@ -74,7 +97,7 @@ async fn weight_consumption(rpc_url: &str) -> Result<WeightConsumption, Box<dyn 
     let consumption = WeightConsumption {
         normal: normal_consumed as f32 / weight_limit as f32,
         operational: operational_consumed as f32 / weight_limit as f32,
-        mandatory: mandatory_consumed as f32 / weight_limit as f32, 
+        mandatory: mandatory_consumed as f32 / weight_limit as f32,
     };
 
     Ok(consumption)
