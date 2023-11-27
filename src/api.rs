890f27f915ec7912816cc9aa06df0203efe783ff
BoredApe8461 <letesinkelegesse@gmail.com>
2023-11-27 13:07:15 +0100

    write

--- src/api.rs
+++ src/api.rs
@@ -1,7 +1,7 @@
 use csv::ReaderBuilder;
 use rocket::{http::Status, response::Responder, serde::json::Json, Request, Response};
 use rocket_cors::CorsOptions;
-use std::{fs::File, io::Read};
+use std::{fs::{OpenOptions, File}, io::{Read, Write, Seek}};
 
 mod parachains;
 
@@ -60,17 +60,30 @@ fn consumption(relay: &str, para_id: ParaId) -> Result<String, Error> {
 
 #[post("/register_para", data = "<para>")]
 fn register_para(para: Json<Parachain>) -> Result<String, Error> {
-    let mut file = File::open(PARACHAINS).map_err(|_| Error::ParasDataNotFound)?;
-    let mut content = String::new();
-    file.read_to_string(&mut content)
-        .map_err(|_| Error::InvalidData)?;
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(PARACHAINS)
+        .map_err(|_| Error::ParasDataNotFound)?;
 
-    let paras: Vec<Parachain> = serde_json::from_str(&content).map_err(|_| Error::InvalidData)?;
+    let mut content = String::new();
+    file.read_to_string(&mut content).map_err(|_| Error::InvalidData)?;
+
+    let mut paras: Vec<Parachain> = serde_json::from_str(&content).map_err(|_| Error::InvalidData)?;
 
     if parachains::parachain(para.relay_chain.clone(), para.para_id.clone()).is_some() {
         return Err(Error::AlreadyRegistered);
     }
-    println!("{:?}", paras);
+
+    paras.push(para.into_inner());
+    let json_data = serde_json::to_string_pretty(&paras).expect("Failed to serialize");
+
+    file.set_len(0).expect("Failed to truncate file");
+    file.seek(std::io::SeekFrom::Start(0)).expect("Failed to seek to the beginning");
+
+
+    file.write_all(json_data.as_bytes()).unwrap();
 
     Ok(format!(""))
 }
