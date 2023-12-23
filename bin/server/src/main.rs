17d01e93762b69ded323eb2470cb6c58ad648e3d
BoredApe8461 <letesinkelegesse@gmail.com>
2023-12-23 11:03:37 +0100

    final touches

--- bin/server/src/main.rs
+++ bin/server/src/main.rs
@@ -1,10 +1,25 @@
+// This file is part of RegionX.
+//
+// RegionX is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// RegionX is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with RegionX.  If not, see <https://www.gnu.org/licenses/>.
+
 /// Web API for interacting with the Consumption Tracker service.
 ///
 /// This API exposes two main endpoints:
 /// - `/consumption`: Used to query consumption data associated with a parachain.
 /// - `/register`: Used to register a parachain for consumption tracking.
 use rocket_cors::CorsOptions;
-use routes::register::register_para;
+use routes::{consumption::consumption, register::register_para};
 
 #[macro_use]
 extern crate rocket;
@@ -13,5 +28,5 @@ extern crate rocket;
 fn rocket() -> _ {
 	rocket::build()
 		.attach(CorsOptions::default().to_cors().unwrap())
-		.mount("/", routes![register_para])
+		.mount("/", routes![consumption, register_para])
 }
--- routes/src/consumption.rs
+++ routes/src/consumption.rs
@@ -1,1 +1,48 @@
+// This file is part of RegionX.
+//
+// RegionX is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 
+// RegionX is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with RegionX.  If not, see <https://www.gnu.org/licenses/>.
+
+use crate::Error;
+use csv::ReaderBuilder;
+use rocket::get;
+use shared::{file_path, parachain};
+use std::fs::File;
+use types::{ParaId, WeightConsumption};
+
+/// Query the consumption data of a parachain.
+///
+/// This will return an error in case there is no data associated with the specific parachain.
+#[get("/consumption/<relay>/<para_id>?<page>&<page_size>")]
+pub fn consumption(
+	relay: &str,
+	para_id: ParaId,
+	page: Option<u32>,
+	page_size: Option<u32>,
+) -> Result<String, Error> {
+	let para = parachain(relay.into(), para_id).ok_or(Error::NotRegistered)?;
+
+	let file = File::open(file_path(para)).map_err(|_| Error::ConsumptionDataNotFound)?;
+	let mut rdr = ReaderBuilder::new().has_headers(false).from_reader(file);
+
+	let (page, page_size) = (page.unwrap_or_default(), page_size.unwrap_or(u32::MAX));
+
+	let weight_consumptions: Vec<WeightConsumption> = rdr
+		.deserialize::<WeightConsumption>()
+		.filter_map(|result| result.ok())
+		.skip((page.saturating_add(page_size)) as usize)
+		.take(page_size as usize)
+		.collect();
+
+	serde_json::to_string(&weight_consumptions).map_err(|_| Error::InvalidData)
+}
