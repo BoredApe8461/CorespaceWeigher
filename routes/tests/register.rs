f75d22d29e865d03036bd7d2de40e8b54b8eefce
BoredApe8461 <letesinkelegesse@gmail.com>
2024-01-02 16:42:12 +0100

    complete register tests

--- routes/tests/register.rs
+++ routes/tests/register.rs
@@ -22,6 +22,7 @@ use rocket::{
 	routes,
 };
 use routes::{register::register_para, Error};
+use shared::registry::{registered_para, registered_paras};
 use types::RelayChain::*;
 
 mod mock;
@@ -29,7 +30,7 @@ use mock::{mock_para, MockEnvironment};
 
 #[test]
 fn register_works() {
-	MockEnvironment::new().execute_with(|| {
+	MockEnvironment::default().execute_with(|| {
 		let rocket = rocket::build().mount("/", routes![register_para]);
 		let client = Client::tracked(rocket).expect("valid rocket instance");
 
@@ -42,10 +43,33 @@ fn register_works() {
 			.dispatch();
 
 		assert_eq!(response.status(), Status::Ok);
+
+		// Ensure the parachain is properly registered:
+		assert_eq!(registered_paras(), vec![para.clone()]);
+		assert_eq!(registered_para(Polkadot, 2001), Some(para));
 	});
 }
 
-fn _parse_err_response<'a>(response: LocalResponse<'a>) -> Error {
+#[test]
+fn cannot_register_same_para_twice() {
+	MockEnvironment::default().execute_with(|| {
+		let rocket = rocket::build().mount("/", routes![register_para]);
+		let client = Client::tracked(rocket).expect("valid rocket instance");
+
+		let para = mock_para(Polkadot, 2001);
+
+		let register = client
+			.post("/register_para")
+			.header(ContentType::JSON)
+			.body(serde_json::to_string(&para).unwrap());
+
+		// Cannot register the same para twice:
+		assert_eq!(register.clone().dispatch().status(), Status::Ok);
+		assert_eq!(parse_err_response(register.dispatch()), Error::AlreadyRegistered);
+	});
+}
+
+fn parse_err_response<'a>(response: LocalResponse<'a>) -> Error {
 	let body = response.into_string().unwrap();
 	body.into()
 }
