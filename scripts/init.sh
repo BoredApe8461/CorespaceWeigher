3fd74f2505655049bc5e3b20d1805c447927af3f
BoredApe8461 <letesinkelegesse@gmail.com>
2024-02-03 09:16:41 +0100

    update scripts

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-TRACKER_LOGS="logs/tracker-logs.out"
-WATCHDOG_LOGS="logs/watchdog-logs.out"
+TRACKER_LOGS_0="logs/tracker-logs-0.out"
+TRACKER_LOGS_1="logs/tracker-logs-1.out"
+
+WATCHDOG_LOGS_0="logs/watchdog-logs-0.out"
+WATCHDOG_LOGS_1="logs/watchdog-logs-1.out"
+
 TRACKER="./target/release/tracker"
+WATCHDOG="scripts/watchdog.sh"
 
 reset_env() {
-    PIDS=$(pgrep -f "$TRACKER")
+    PIDS=$(pgrep -f "$TRACKER|$WATCHDOG")
 
     if [ -z "$PIDS" ]; then
         echo "Process not found."
@@ -20,6 +25,8 @@ reset_env() {
 reset_env
 
 # start the tracker again
-nohup sh -c 'RUST_LOG=INFO ./target/release/tracker' > $TRACKER_LOGS 2>&1 &
+nohup sh -c 'RUST_LOG=INFO ./target/release/tracker --rpc-index 0' > $TRACKER_LOGS_0 2>&1 &
+nohup sh -c 'RUST_LOG=INFO ./target/release/tracker --rpc-index 1' > $TRACKER_LOGS_1 2>&1 &
 # start the watchdog
-nohup sh -c 'scripts/watchdog.sh' > $WATCHDOG_LOGS 2>&1 &
+nohup sh -c 'scripts/watchdog.sh "$1"' _ 0 > $WATCHDOG_LOGS_0 2>&1 &
+nohup sh -c 'scripts/watchdog.sh "$1"' _ 1 > $WATCHDOG_LOGS_1 2>&1 &
--- scripts/watchdog.sh
+++ scripts/watchdog.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
-FILE_TO_WATCH="logs/tracker-logs.out"
+FILE_TO_WATCH="logs/tracker-logs-$1.out"
 
 WS_CONNECTION_ERROR="Failed to read message: Networking or low-level protocol error: WebSocket connection error: connection closed"
-TRACKER="./target/release/tracker"
-
-restart_tracker() {
-    PIDS=$(pgrep -f "$TRACKER")
-
-    if [ -z "$PIDS" ]; then
-        echo "Process not found."
-    else
-        # Kill each process
-        for PID in $PIDS; do
-            kill -9 $PID
-        done
-
-        # start the tracker again
-        nohup sh -c 'RUST_LOG=INFO ./target/release/tracker' > $FILE_TO_WATCH 2>&1 &
-    fi
-}
 
 # Continuously watch the file
 tail -f "$FILE_TO_WATCH" | while read LINE
@@ -27,6 +10,6 @@ do
     echo "$LINE" | grep "$WS_CONNECTION_ERROR" > /dev/null
     if [ $? = 0 ]
     then
-       restart_tracker 
+        ./scripts/init.sh
     fi
 done
